1afe079d79fa3929a54cf8c9b7d79330e376ac50
BrendowLincoln <linconlbrendow@gmail.com>
2022-09-20 21:24:02 -0300

    LS-1-0: Creating persistence strategy class.

---
+++ src/main/java/br/edu/femass/daos/Persistence.java
@@ -0,0 +1,17 @@
+package br.edu.femass.daos;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class Persistence {
+
+    protected ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
+}
