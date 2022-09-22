1a548c4cf560a956e69d50cdf0d31e2124c72256
BrendowLincoln <linconlbrendow@gmail.com>
2022-09-21 23:19:07 -0300

    LS-2-0: Creating Author model and AuthorDao class. Implementing methods save and getAll in AuthorDao.

---
+++ src/main/java/br/edu/femass/daos/AuthorDao.java
@@ -0,0 +1,42 @@
+package br.edu.femass.daos;
+
+import br.edu.femass.models.Author;
+import java.util.List;
+
+public class AuthorDao extends Persistence implements Dao<Author> {
+    private final String FILE_NAME = GlobalConstants.PERSISTENCE_DIRECTORY_PATH + "authors.json";
+
+    @Override
+    public void save(Author author) throws Exception {
+        List<Author> authors = getAll();
+        authors.add(author);
+        String json = getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(authors);
+
+        FileOutputStream out = new FileOutputStream(FILE_NAME);
+        out.write(json.getBytes());
+        out.close();
+    }
+
+    @Override
+    public List<Author> getAll() throws Exception {
+        try {
+            FileInputStream in = new FileInputStream(FILE_NAME);
+            String json = new String(in.readAllBytes());
+            in.close();
+            List<Author> authors = getObjectMapper().readValue(json, new TypeReference<List<Author>>(){});
+            return authors;
+        } catch (FileNotFoundException f) {
+            return new ArrayList<Author>();
+        }
+    }
+
+    @Override
+    public void update(Author author) throws Exception {
+
+    }
+
+    @Override
+    public void delete() throws Exception {
+
+    }
+}
---
+++ src/main/java/br/edu/femass/models/Author.java
@@ -0,0 +1,52 @@
+package br.edu.femass.models;
+
+public class Author {
+    private Integer code;
+    private String name;
+    private String secondName;
+    private String nationality;
+
+    private Integer _initialCode = 1;
+
+    public Author() { }
+
+    public Author(
+        String name,
+        String secondName,
+        String nationality
+    ) {
+        this.code = _initialCode;
+        this.name = name;
+        this.secondName = secondName;
+        this.nationality = nationality;
+
+        this._initialCode++;
+    }
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSecondName() {
+        return this.secondName;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.secondName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Author author = (Author) object;
+        return this.code.equals(author.getCode().toString());
+    }
+}
