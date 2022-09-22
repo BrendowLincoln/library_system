51913f84785df95889850168aeee54aad85ad825
BrendowLincoln <linconlbrendow@gmail.com>
2022-09-21 23:00:01 -0300

    Main - Initial the main ApplicationView desing.

---
+++ src/main/java/br/edu/femass/ApplicationView.java
@@ -0,0 +1,17 @@
+package br.edu.femass;
+
+import javax.swing.*;
+
+public class ApplicationView {
+    private JPanel applicationPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+
+
+    public JPanel getApplicationPanel() {
+        return this.applicationPanel;
+    }
+}
