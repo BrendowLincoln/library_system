6e53329885328ff1fe122003cf83365bca8a5c53
BrendowLincoln <linconlbrendow@gmail.com>
2022-09-21 22:59:03 -0300

    Main - Change Application class for Startup class.

--- src/main/java/br/edu/femass/Application.java
+++
@@ -1,9 +0,0 @@
-package br.edu.femass;
-
-import br.edu.femass.daos.Persistence;
-
-public class Application {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/br/edu/femass/Startup.java
@@ -0,0 +1,28 @@
+package br.edu.femass;
+
+import br.edu.femass.daos.AuthorDao;
+import br.edu.femass.models.Author;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Startup {
+
+    public static void main(String[] args) {
+        final ApplicationView application = new ApplicationView();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final Integer screenWidth = (int) (screenSize.getWidth() * 0.95);
+        final Integer screenHeight = (int) (screenSize.getHeight() * 0.8);
+
+        JFrame applicationFrame = new JFrame("Livraría FeMASS");
+        applicationFrame.setContentPane(application.getApplicationPanel());
+        applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        applicationFrame.pack();
+        applicationFrame.setSize(screenWidth, screenHeight);
+        applicationFrame.setVisible(true);
+        applicationFrame.setLocation(
+            screenSize.width/2- applicationFrame.getSize().width/2,
+            screenSize.height/2-applicationFrame.getSize().height/2
+        );
+    }
+}
