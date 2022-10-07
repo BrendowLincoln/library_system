54eeb14f5f7d8088f1d6329d92d77d58fe8b1daf
BrendowLincoln <linconlbrendow@gmail.com>
2022-10-07 01:17:04 -0300

    Main - Creating custom Dao exception and message class to Author Dao methods. Creation save methods.

--- src/main/java/br/edu/femass/daos/AuthorDao.java
+++ src/main/java/br/edu/femass/daos/AuthorDao.java
@@ -2,6 +2,8 @@ package br.edu.femass.daos;
 
 import br.edu.femass.models.Author;
 import br.edu.femass.utils.GlobalConstants;
+import br.edu.femass.utils.exceptions.AuthorDaoExceptionMessage;
+import br.edu.femass.utils.exceptions.DaoException;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.FileInputStream;
@@ -12,19 +14,37 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class AuthorDao extends Persistence implements Dao<Author> {
-    private final String FILE_NAME = GlobalConstants.PERSISTENCE_DIRECTORY_PATH + "authors.json";
+    private String _fileName;
+
+
+    public AuthorDao() {
+        _fileName = GlobalConstants.PERSISTENCE_DIRECTORY_PATH + "authors.json";
+    }
+
+    public AuthorDao(String pathToStorage) {
+        _fileName = pathToStorage;
+    }
 
     @Override
-    public void save(Author author) throws Exception {
-        List<Author> authors = getAll();
-        authors.add(author);
-        writeInFile(authors);
+    public void save(Author author) throws DaoException {
+        try {
+
+            if(author.getCode() == null || author.getName().isEmpty() || author.getSecondName().isEmpty() || author.getNationality().isEmpty()) {
+                throw new DaoException(AuthorDaoExceptionMessage.COULD_NOT_SAVE_AUTHOR);
+            }
+
+            List<Author> authors = getAll();
+            authors.add(author);
+            writeInFile(authors);
+        } catch (Exception de) {
+            throw new DaoException(AuthorDaoExceptionMessage.COULD_NOT_SAVE_AUTHOR);
+        }
     }
 
     @Override
     public List<Author> getAll() throws Exception {
         try {
-            FileInputStream in = new FileInputStream(FILE_NAME);
+            FileInputStream in = new FileInputStream(_fileName);
             String json = new String(in.readAllBytes());
             in.close();
             List<Author> authors = getObjectMapper().readValue(json, new TypeReference<List<Author>>(){});
@@ -35,7 +55,7 @@ public class AuthorDao extends Persistence implements Dao<Author> {
     }
 
     @Override
-    public Author getByCode(Integer code) throws Exception {
+    public Author getByCode(Long code) throws Exception {
         try {
             List<Author> authors = getAll();
             if (authors == null || authors.isEmpty()) {
@@ -73,9 +93,9 @@ public class AuthorDao extends Persistence implements Dao<Author> {
     }
 
 
-    private void writeInFile(List<Author> authors) throws Exception {
+    private void writeInFile(List<Author> authors) throws Exception  {
         String json = getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(authors);
-        FileOutputStream out = new FileOutputStream(FILE_NAME);
+        FileOutputStream out = new FileOutputStream(_fileName);
         out.write(json.getBytes());
         out.close();
     }
